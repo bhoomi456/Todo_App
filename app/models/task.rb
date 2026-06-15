f00f6c0148389d5e82de457ffa2109f64d3fac2a
bhoomi456 <bhoomij0304@gmail.com>
2026-06-15 13:18:26 +0530

    adding callbacks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,52 @@
 class Task < ApplicationRecord
+  validates :name ,presence: true, uniqueness: true
+
+  before_validation :set_default_name
+  after_validation :show_validation_status
+  before_save :capitalize_name
+  after_save {puts "After save successfully"}
+  before_create :set_default_description
+  after_create {puts "After create callback"}
+  before_update {puts "Before update callback"}
+  after_update {puts"After update callback"}
+  before_destroy {puts "Before destory callback"}
+  after_destroy {puts "After destroy callback"}
+  around_save :log_save
+  before_commit {puts "Before commit callback"}
+  after_commit {puts "After commit Task permanently saved"}
+
+  private
+
+  def set_default_name
+    self.name = "Untitled" if name.blank?
+    puts "Before validation"
+  end
+
+  def show_validation_status
+    if errors.any?
+      puts "Validation Failed:"
+      puts errors.full_messages
+    else
+      puts "Validation Passed"
+    end
+  end
+
+  def capitalize_name
+    self.name = name.capitalize
+    puts "Before save callback"
+  end
+
+  def set_default_description
+    self.description = "Working on task" if description.blank?
+    puts "Before create callback"
+  end
+
+  def log_save
+    puts "Before arround save"
+
+    yield
+
+    puts "After arround save"
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_09_100009) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_11_120840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_09_100009) do
   end
 
   create_table "tasks", force: :cascade do |t|
+    t.integer "age"
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
