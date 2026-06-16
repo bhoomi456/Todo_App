8e9a1625e709e4694b2d56821948683694e21f3b
bhoomi456 <bhoomij0304@gmail.com>
2026-06-16 14:23:17 +0530

    add route todolists

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,4 +1,5 @@
 class TodoListsController < ApplicationController
+  before_action :set_todo_list, only: %i[show]
 
   def index
     @todo_lists = TodoList.all
@@ -8,10 +9,31 @@ class TodoListsController < ApplicationController
   end
 
   def new
-    @todo_lists = TodoList.new
+    @todo_list = TodoList.new
   end
 
+  def create
+    @todo_list = TodoList.new(todo_lists_params)
+
+    if @todo_list.save
+      redirect_to todo_lists_path, notice "Todo list created successfully"
+    else
+      render :new, status: :unprocessable_entity
+  end
+
+
   def edit
+  end
+
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
+
+  def todo_lists_params
+    params.require(:todo_list).permit(:name,:description)
   end
 
 end
